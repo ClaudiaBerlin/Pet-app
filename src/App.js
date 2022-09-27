5f64a41cf40a47f5bb44dcba30e7cfa38a77917a
ClaudiaBerlin <89379833+analogqueen@users.noreply.github.com>
2022-09-27 09:10:00 +0200

    first kompenent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Hello from "./Hello";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
+        <Hello />
+        <Hello />
+        <Hello />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/Hello.jsx
@@ -0,0 +1,17 @@
+// sfc befehl ausführen, eine komponente in React bitte immer in Großbuchstaben beginnen
+
+const  Hello = () => {
+    // die komponenente gibt ein h1 Element zurück
+    return (
+        <div>
+        <h1>Hallo Welt</h1> ;
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas corrupti nobis pariatur nisi corporis expedita ipsam error molestiae minus eligendi. Dolorem rem officiis nihil adipisci nostrum quisquam exercitationem dolorum blanditiis.</p>
+        </div>
+    )
+
+  
+   
+}
+ 
+// exportiert die komponenente
+export default Hello;
